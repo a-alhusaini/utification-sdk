e4defb32b22be43e0aad2816c658ec38eacf369a
Abdullah Alhusaini <arvg3a@gmail.com>
2021-02-21 05:43:00 +0300

    added documentation to function

--- src/utils/localFetch.js
+++ src/utils/localFetch.js
@@ -1,3 +1,8 @@
+/**
+ *
+ * @param {string} location the api route to fetch
+ * @param body the fetch request body (includes headers/request body/method etc)
+ */
 export default function localFetch(location, body) {
   body.data = body.body;
   return axios({ url: this.apiOrigin + location, ...body });
