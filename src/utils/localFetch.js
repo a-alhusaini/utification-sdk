b92efe2b53c1536f4245b6c10a4642dc9b62f820
Abdullah Alhusaini <arvg3a@gmail.com>
2021-03-22 08:48:25 +0300

    v0.0.23

--- src/utils/localFetch.js
+++ src/utils/localFetch.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 /**
+ * @param {string} apiOrigin the place where the call is supposed to be sent to
  * @param {string} location the api route to fetch
  * @param body the fetch request body (includes headers/request body/method etc)
  */
 export default function localFetch(apiOrigin, location, body) {
   body.data = body.body;
-  return axios({ url: this.apiOrigin + location, ...body });
+  return axios({ url: apiOrigin + location, ...body });
 }
